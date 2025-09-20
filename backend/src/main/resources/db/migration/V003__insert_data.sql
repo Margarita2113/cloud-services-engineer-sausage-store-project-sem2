82120bb5d71bad7bab854a01261805739201585d
Margo <rita_2113@mail.ru>
2025-09-20 15:54:27 +0300

    add autoscaling

--- backend/src/main/resources/db/migration/V003__insert_data.sql
+++ backend/src/main/resources/db/migration/V003__insert_data.sql
@@ -1,22 +1,15 @@
--- Заполним таблицы
-INSERT INTO product (id, name, picture_url, price) VALUES (1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00);
-INSERT INTO product (id, name, picture_url, price) VALUES (2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00);
-INSERT INTO product (id, name, picture_url, price) VALUES (3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00);
-INSERT INTO product (id, name, picture_url, price) VALUES (4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00);
-INSERT INTO product (id, name, picture_url, price) VALUES (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00);
-INSERT INTO product (id, name, picture_url, price) VALUES (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
+INSERT INTO product (id, name, picture_url, price) VALUES
+                                                       (1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00),
+                                                       (2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00),
+                                                       (3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00),
+                                                       (4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00),
+                                                       (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
+                                                       (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
 
--- Заполнение таблицы orders (100 случайных записей)
-INSERT INTO orders (status, date_created)
-SELECT 
-    (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], 
-    NOW() - (random() * INTERVAL '90 days')
-FROM generate_series(1, 100);
+INSERT INTO orders (id, status, date_created)
+SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW())))
+FROM generate_series(1, 10000) s(i);
 
--- Заполнение таблицы order_product (связь товаров с заказами)
 INSERT INTO order_product (quantity, order_id, product_id)
-SELECT 
-    floor(1 + random() * 10)::int,  -- Количество от 1 до 10
-    (SELECT id FROM orders ORDER BY random() LIMIT 1), 
-    (SELECT id FROM product ORDER BY random() LIMIT 1)
-FROM generate_series(1, 200); -- 200 случайных связей
+SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6
+FROM generate_series(1, 10000) s(i);
